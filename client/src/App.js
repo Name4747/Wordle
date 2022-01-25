14c4c03198ea3eb196e8cfdff333bf367d3da374
Name4747 <684438@sylvaniastudents.org>
2022-01-25 13:25:52 -0500

    define newLetter

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,15 @@ function App() {
       letter: l,
       color: "rgb(129,131,132)"
     }))
-    const [letters,setLetters] = useState(alphaArr)
+  const [letters,setLetters] = useState(alphaArr)
+  const [newLetter,setNewLetter] = useState('')
   return (
     <div>
       <header>
         <h1 id="title">WORDLE</h1>
       </header>
-      <Board letters={letters} setLetters={setLetters}/>
-      <Keyboard letters={letters} />
+      <Board letters={letters} setLetters={setLetters} newLetter={newLetter}/>
+      <Keyboard letters={letters} setNewLetter={setNewLetter}/>
     </div>
   );
 }
--- client/src/Key.js
+++ client/src/Key.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Key = ({letter,color}) => {
+    const onKeyPress = () => {
+        
+    }
     return (
         <div className="key" style={{backgroundColor: color}}>
             {letter}
